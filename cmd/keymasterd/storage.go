Cloud-Foundations/keymaster

commit b42d1e1d5335d8051c2de62da9b3bba2117965ab

adding and removing users no tests

cmd/keymasterd/storage.go
@@ -223,6 +223,14 @@ func copyDBIntoSQLite(source, destination *sql.DB, destinationType string) error
 		logger.Printf("err='%s'", err)
 		return err
 	}
+
+	deleteProfilesQueryStr := fmt.Sprintf("DELETE from user_profile ")
+	_, err = destination.Query(deleteProfilesQueryStr)
+	if err != nil {
+		logger.Printf("err='%s'", err)
+		return err
+	}
+
 	stmtText := saveUserProfileStmt[destinationType]
 	stmt, err := tx.Prepare(stmtText)
 	if err != nil {
@@ -494,6 +502,35 @@ func (state *RuntimeState) SaveUserProfile(username string, profile *userProfile
 		return err
 	}
 	metricLogExternalServiceDuration("storage-save", time.Since(start))
+	return nil
+}
+
+var deleteUserProfileStmt = map[string]string{
+	"sqlite":   "delete from  user_profile where username = ?",
+	"postgres": "delete from  user_profile where username = $1",
+}
+
+func (state *RuntimeState) DeleteUserProfile(username string) error {
+	//delete from DB
+	tx, err := state.db.Begin()
+	if err != nil {
+		return err
+	}
+	stmtText := deleteUserProfileStmt[state.dbType]
+	stmt, err := tx.Prepare(stmtText)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(username)
+	if err != nil {
+		return err
+	}
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 

cmd/keymasterd/adminHandlers.go
@@ -1,0 +1,187 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"regexp"
+
+	"github.com/Cloud-Foundations/keymaster/lib/instrumentedwriter"
+)
+
+const usersPath = "/users/"
+const addUsersPath = "/admin/adduser"
+const deleteUsersPath = "/admin/deleteuser"
+
+func (state *RuntimeState) usersHandler(
+	w http.ResponseWriter, r *http.Request) {
+	if state.sendFailureToClientIfLocked(w, r) {
+		return
+	}
+	authUser, _, err := state.checkAuth(w, r, state.getRequiredWebUIAuthLevel())
+	if err != nil {
+		logger.Debugf(1, "%v", err)
+		return
+	}
+	w.(*instrumentedwriter.LoggingWriter).SetUsername(authUser)
+
+	if !state.IsAdminUser(authUser) {
+		state.writeFailureResponse(w, r, http.StatusUnauthorized, "")
+		return
+	}
+
+	users, _, err := state.GetUsers()
+	if err != nil {
+		logger.Printf("Getting users error: %v", err)
+		http.Error(w, "error", http.StatusInternalServerError)
+		return
+
+	}
+
+	JSSources := []string{"/static/jquery-3.4.1.min.js"}
+
+	displayData := usersPageTemplateData{
+		AuthUsername: authUser,
+		Title:        "Keymaster Users",
+		Users:        users,
+		JSSources:    JSSources}
+	err = state.htmlTemplate.ExecuteTemplate(w, "usersPage", displayData)
+	if err != nil {
+		logger.Printf("Failed to execute %v", err)
+		http.Error(w, "error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (state *RuntimeState) sendFailureToClientIfNonAdmin(
+	w http.ResponseWriter, r *http.Request) bool {
+	if state.sendFailureToClientIfLocked(w, r) {
+		return true
+	}
+	authUser, _, err := state.checkAuth(w, r, state.getRequiredWebUIAuthLevel())
+	if err != nil {
+		logger.Debugf(1, "%v", err)
+		state.writeFailureResponse(w, r, http.StatusInternalServerError, "")
+		return true
+	}
+	w.(*instrumentedwriter.LoggingWriter).SetUsername(authUser)
+	if !state.IsAdminUser(authUser) {
+		state.writeFailureResponse(w, r, http.StatusUnauthorized, "")
+		return true
+	}
+	return false
+}
+
+func (state *RuntimeState) addUsersHandler(
+	w http.ResponseWriter, r *http.Request) {
+	if state.sendFailureToClientIfNonAdmin(w, r) {
+		return
+	}
+	//TODO: check method, must be POST
+	if r.Method != "POST" {
+		state.writeFailureResponse(w, r, http.StatusMethodNotAllowed, "")
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		logger.Printf("error parsing err=%s", err)
+		state.writeFailureResponse(w, r, http.StatusInternalServerError, "")
+		return
+	}
+	usersToAdd, ok := r.Form["username"]
+	if !ok {
+		state.writeFailureResponse(w, r, http.StatusBadRequest, "Required Parameters missing")
+		return
+	}
+	//It is a good idea to match?
+	for _, username := range usersToAdd {
+		matched, err := regexp.Match(`^[A-Za-z0-9-_.]+$`, []byte(username))
+		if err != nil {
+			logger.Printf("error parsing err=%s", err)
+			state.writeFailureResponse(w, r, http.StatusInternalServerError, "")
+			return
+		}
+		if !matched {
+			state.writeFailureResponse(w, r, http.StatusBadRequest, "Invalid Usernames found")
+			return
+		}
+	}
+	var newProfiles map[string]*userProfile
+	newProfiles = make(map[string]*userProfile)
+	//check if usernames already exist
+	for _, username := range usersToAdd {
+		profile, existing, fromCache, err := state.LoadUserProfile(username)
+		if err != nil {
+			logger.Printf("error parsing err=%s", err)
+			state.writeFailureResponse(w, r, http.StatusInternalServerError, "")
+			return
+		}
+		if existing {
+			state.writeFailureResponse(w, r, http.StatusBadRequest, "User exist in DB")
+			return
+		}
+		if fromCache {
+			state.writeFailureResponse(w, r, http.StatusServiceUnavailable, "Working in db disconnected mode, try again later")
+			return
+		}
+		newProfiles[username] = profile
+	}
+	for username, userProfile := range newProfiles {
+		err := state.SaveUserProfile(username, userProfile)
+		if err != nil {
+			logger.Printf("error Savinf Profile  err=%s", err)
+			state.writeFailureResponse(w, r, http.StatusInternalServerError, "")
+			return
+		}
+	}
+	//if html then redirect to users page, else return jsonOK
+	preferredAcceptType := getPreferredAcceptType(r)
+	switch preferredAcceptType {
+	case "text/html":
+
+		http.Redirect(w, r, usersPath, http.StatusFound)
+	default:
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "OK\n")
+	}
+}
+
+func (state *RuntimeState) deleteUsersHandler(
+	w http.ResponseWriter, r *http.Request) {
+	if state.sendFailureToClientIfNonAdmin(w, r) {
+		return
+	}
+	//TODO: check method, must be POST
+	if r.Method != "POST" {
+		state.writeFailureResponse(w, r, http.StatusMethodNotAllowed, "")
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		logger.Printf("error parsing err=%s", err)
+		state.writeFailureResponse(w, r, http.StatusInternalServerError, "")
+		return
+	}
+	usersToDelete, ok := r.Form["username"]
+	if !ok {
+		state.writeFailureResponse(w, r, http.StatusBadRequest, "Required Parameters missing")
+		return
+	}
+	for _, username := range usersToDelete {
+		err = state.DeleteUserProfile(username)
+		if err != nil {
+			logger.Printf("error parsing err=%s", err)
+			state.writeFailureResponse(w, r, http.StatusInternalServerError, "")
+			return
+		}
+	}
+	preferredAcceptType := getPreferredAcceptType(r)
+	switch preferredAcceptType {
+	case "text/html":
+		http.Redirect(w, r, usersPath, http.StatusFound)
+	default:
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "OK\n")
+	}
+}

cmd/keymasterd/app.go
@@ -61,6 +61,7 @@ const (
 	AuthTypeIPCertificate
 	AuthTypeTOTP
 	AuthTypeOkta2FA
+	AuthTypeBootstrapOTP
 )
 
 const AuthTypeAny = 0xFFFF
@@ -112,12 +113,18 @@ type totpAuthData struct {
 	ValidatorAddr   string
 }
 
+type bootstrapOTPData struct {
+	CreatedAt time.Time
+	Value     string
+}
+
 type userProfile struct {
 	U2fAuthData                map[int64]*u2fAuthData
 	RegistrationChallenge      *u2f.Challenge
 	PendingTOTPSecret          *[][]byte
 	LastSuccessfullTOTPCounter int64
 	TOTPAuthData               map[int64]*totpAuthData
+	bootstrapOTP               bootstrapOTPData
 }
 
 type localUserData struct {
@@ -1145,43 +1152,6 @@ func (state *RuntimeState) IsAdminUserAndU2F(user string, loginLevel int) bool {
 	return state.IsAdminUser(user) && ((loginLevel & AuthTypeU2F) != 0)
 }
 
-const usersPath = "/users/"
-
-func (state *RuntimeState) usersHandler(
-	w http.ResponseWriter, r *http.Request) {
-	if state.sendFailureToClientIfLocked(w, r) {
-		return
-	}
-	authUser, _, err := state.checkAuth(w, r, state.getRequiredWebUIAuthLevel())
-	if err != nil {
-		logger.Debugf(1, "%v", err)
-		return
-	}
-	w.(*instrumentedwriter.LoggingWriter).SetUsername(authUser)
-
-	users, _, err := state.GetUsers()
-	if err != nil {
-		logger.Printf("Getting users error: %v", err)
-		http.Error(w, "error", http.StatusInternalServerError)
-		return
-
-	}
-
-	JSSources := []string{"/static/jquery-3.4.1.min.js"}
-
-	displayData := usersPageTemplateData{
-		AuthUsername: authUser,
-		Title:        "Keymaster Users",
-		Users:        users,
-		JSSources:    JSSources}
-	err = state.htmlTemplate.ExecuteTemplate(w, "usersPage", displayData)
-	if err != nil {
-		logger.Printf("Failed to execute %v", err)
-		http.Error(w, "error", http.StatusInternalServerError)
-		return
-	}
-}
-
 const profilePath = "/profile/"
 
 func profileURI(authUser, assumedUser string) string {
@@ -1537,6 +1507,8 @@ func main() {
 	serviceMux.HandleFunc(logoutPath, runtimeState.logoutHandler)
 	serviceMux.HandleFunc(profilePath, runtimeState.profileHandler)
 	serviceMux.HandleFunc(usersPath, runtimeState.usersHandler)
+	serviceMux.HandleFunc(addUsersPath, runtimeState.addUsersHandler)
+	serviceMux.HandleFunc(deleteUsersPath, runtimeState.deleteUsersHandler)
 
 	serviceMux.HandleFunc(idpOpenIDCConfigurationDocumentPath, runtimeState.idpOpenIDCDiscoveryHandler)
 	serviceMux.HandleFunc(idpOpenIDCJWKSPath, runtimeState.idpOpenIDCJWKSHandler)

cmd/keymasterd/config.go
@@ -69,6 +69,7 @@ type baseConfig struct {
 	AutomationUsers              []string   `yaml:"automation_users"`
 	DisableUsernameNormalization bool       `yaml:"disable_username_normalization"`
 	EnableLocalTOTP              bool       `yaml:"enable_local_totp"`
+	EnableBootstrapOTP           bool       `yaml:"enable_bootstrapotp"`
 }
 
 type GitDatabaseConfig struct {

cmd/keymasterd/templateData.go
@@ -222,6 +222,14 @@ const usersHTML = `
        <li><a href="/profile/{{.}}">{{.}}</a></li>
     {{end}}
     </ul>
+    <br>
+    <h3>Manage Users </h3>
+    <form enctype="application/x-www-form-urlencoded" action="/admin/adduser" method="post">
+       <p>Username: <INPUT TYPE="text" NAME="username" SIZE=18  autocomplete="off"></p>
+       <p><input type="submit" value="Add User" /> </p>
+       <p><input type="submit" value="Delete User" formaction="/admin/deleteuser" /> </p>
+    </form>
+
     </div>
     {{template "footer" . }}
     </div>

commit b962c888073a24137cef23ee0288db2d24a8a164

Fix file descriptor leak introduced by b42d1e1d5335d805.

cmd/keymasterd/storage.go
@@ -225,10 +225,11 @@ func copyDBIntoSQLite(source, destination *sql.DB, destinationType string) error
 	}
 
 	deleteProfilesQueryStr := fmt.Sprintf("DELETE from user_profile ")
-	_, err = destination.Query(deleteProfilesQueryStr)
-	if err != nil {
+	if rows, err := destination.Query(deleteProfilesQueryStr); err != nil {
 		logger.Printf("err='%s'", err)
 		return err
+	} else {
+		rows.Close()
 	}
 
 	stmtText := saveUserProfileStmt[destinationType]