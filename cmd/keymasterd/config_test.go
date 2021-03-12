22e827fba01652813aeeb12f26a248da55b39387
Camilo Viecco <cviecco@purestorage.com>
2021-03-12 09:01:49 -0800

    now with config tests

--- cmd/keymasterd/config_test.go
+++ cmd/keymasterd/config_test.go
@@ -11,6 +11,11 @@ import (
 	"github.com/Cloud-Foundations/golib/pkg/log/testlogger"
 )
 
+//openssl genpkey  -algorithm ED25519 -out key.pem
+const pkcs8Ed25519PrivateKey = `-----BEGIN PRIVATE KEY-----
+MC4CAQAwBQYDK2VwBCIEIHoHbl2RwHwmyWtXVLroUZEI+d/SqL3RKmECM5P7o7D5
+-----END PRIVATE KEY-----`
+
 func TestGenerateNewConfigInternal(t *testing.T) {
 	t.Logf("hello")
 	dir, err := ioutil.TempDir("", "config_testing")
@@ -40,5 +45,38 @@ func TestGenerateNewConfigInternal(t *testing.T) {
 	}
 
 	// TODO: test decrypt file
+}
+
+func TestLoadSignersFromPemData(t *testing.T) {
+	//testSignerPrivateKey
+	state := RuntimeState{logger: testlogger.New(t)}
+	//expect success just signer key
+	err := state.loadSignersFromPemData([]byte(testSignerPrivateKey), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = state.loadSignersFromPemData([]byte(testSignerPrivateKey), []byte(pkcs8Ed25519PrivateKey))
+	if err != nil {
+		t.Fatal(err)
+	}
+	// now failure ... the signer is ed25519 which is not compatible with x509
+	err = state.loadSignersFromPemData([]byte(pkcs8Ed25519PrivateKey), []byte(pkcs8Ed25519PrivateKey))
+	if err == nil {
+		t.Fatalf("Should have failed because signer is NOT a compatible key")
+	}
+	// another failure, both are RSA
+	err = state.loadSignersFromPemData([]byte(testSignerPrivateKey), []byte(testSignerPrivateKey))
+	if err == nil {
+		t.Fatalf("Should have failed because passed edsigner is RSA not ed25519")
+	}
+	// another failure, signer is not even pem
+	err = state.loadSignersFromPemData([]byte("hello"), []byte(testSignerPrivateKey))
+	if err == nil {
+		t.Fatalf("Should have failed because passed sginer data is not even pem")
+	}
+	err = state.loadSignersFromPemData([]byte(testSignerPrivateKey), []byte("hello"))
+	if err == nil {
+		t.Fatalf("Should have failed because signer ed signer is not even pem")
+	}
 
 }
