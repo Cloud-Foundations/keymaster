a5f69a794e1248a8bc028bd3118b1217a1dd2ce3
Richard Gooch <rg+github0@safe-mbox.com>
2021-08-30 08:34:20 -0700

    Add lib/client/aws_role:GetRoleCertificateTLS().

--- lib/client/aws_role/api.go
+++ lib/client/aws_role/api.go
@@ -3,9 +3,13 @@ package aws_role
 import (
 	"context"
 	"crypto"
+	"crypto/tls"
 	"net/http"
 
 	"github.com/Cloud-Foundations/golib/pkg/log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
 type Params struct {
@@ -17,12 +21,22 @@ type Params struct {
 	HttpClient *http.Client
 	KeyType    string // "RSA"
 	Signer     crypto.Signer
+	awsConfig  aws.Config
 	derPubKey  []byte
+	isSetup    bool
 	pemPubKey  []byte
+	roleArn    string
+	stsClient  *sts.Client
 }
 
 // GetRoleCertificate requests an AWS role identify certificate from the
 // Keymaster server specified in params. It returns the certificate PEM.
 func GetRoleCertificate(params Params) ([]byte, error) {
 	return params.getRoleCertificate()
+}
+
+// GetRoleCertificateTLS requests an AWS role identify certificate from the
+// Keymaster server specified in params. It returns the certificate.
+func GetRoleCertificateTLS(params Params) (*tls.Certificate, error) {
+	return params.getRoleCertificateTLS()
 }
--- lib/client/aws_role/impl.go
+++ lib/client/aws_role/impl.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
@@ -39,84 +40,71 @@ func parseArn(arnString string) (*arn.ARN, error) {
 	return &parsedArn, nil
 }
 
-// Returns certificate PEM block.
-func (p Params) getRoleCertificate() ([]byte, error) {
-	if p.KeymasterServer == "" {
-		return nil, fmt.Errorf("no keymaster server specified")
-	}
-	if p.Logger == nil {
-		return nil, fmt.Errorf("no logger specified")
-	}
-	if p.Context == nil {
-		p.Context = context.TODO()
-	}
-	if p.HttpClient == nil {
-		p.HttpClient = http.DefaultClient
-	}
-	if p.Signer == nil {
-		signer, err := rsa.GenerateKey(rand.Reader, 2048)
-		if err != nil {
-			return nil, err
-		}
-		p.Signer = signer
-		p.KeyType = "RSA"
-	}
-	if p.KeyType != "RSA" {
-		return nil, fmt.Errorf("unsupported key type: %s", p.KeyType)
-	}
-	derPubKey, err := x509.MarshalPKIXPublicKey(p.Signer.Public())
-	if err != nil {
-		return nil, err
-	}
-	p.derPubKey = derPubKey
-	awsConfig, err := config.LoadDefaultConfig(p.Context)
-	if err != nil {
-		return nil, err
-	}
-	p.pemPubKey = pem.EncodeToMemory(&pem.Block{
-		Bytes: p.derPubKey,
-		Type:  "PUBLIC KEY",
-	})
-	stsClient := sts.NewFromConfig(awsConfig)
-	idOutput, err := stsClient.GetCallerIdentity(p.Context,
-		&sts.GetCallerIdentityInput{})
-	if err != nil {
-		return nil, err
-	}
-	p.Logger.Debugf(0, "Account: %s, ARN: %s, UserId: %s\n",
-		*idOutput.Account, *idOutput.Arn, *idOutput.UserId)
-	parsedArn, err := parseArn(*idOutput.Arn)
-	if err != nil {
-		return nil, err
-	}
-	assumeOutput, err := stsClient.AssumeRole(p.Context, &sts.AssumeRoleInput{
+func (p *Params) getCredentials() (aws.Credentials, error) {
+	assumeOutput, err := p.stsClient.AssumeRole(p.Context, &sts.AssumeRoleInput{
 		DurationSeconds: aws.Int32(900),
-		RoleArn:         aws.String(parsedArn.String()),
+		RoleArn:         aws.String(p.roleArn),
 		RoleSessionName: aws.String("identity-verifier"),
 	})
 	if err != nil {
 		p.Logger.Println(err)
-		creds, err := awsConfig.Credentials.Retrieve(p.Context)
+		creds, err := p.awsConfig.Credentials.Retrieve(p.Context)
 		if err != nil {
-			return nil, err
+			return aws.Credentials{}, err
 		}
 		p.Logger.Println(
 			"unable to assume limited role, passing primary credentials")
-		return p.requestCertificate(creds.AccessKeyID, creds.SecretAccessKey,
-			creds.SessionToken)
+		return creds, nil
 	}
-	return p.requestCertificate(*assumeOutput.Credentials.AccessKeyId,
-		*assumeOutput.Credentials.SecretAccessKey,
-		*assumeOutput.Credentials.SessionToken)
+	return aws.Credentials{
+			AccessKeyID:     *assumeOutput.Credentials.AccessKeyId,
+			SecretAccessKey: *assumeOutput.Credentials.SecretAccessKey,
+			SessionToken:    *assumeOutput.Credentials.SessionToken},
+		nil
 }
 
 // Returns certificate PEM block.
-func (p *Params) requestCertificate(key, secret, token string) ([]byte, error) {
+func (p *Params) getRoleCertificate() ([]byte, error) {
+	if err := p.setupVerify(); err != nil {
+		return nil, err
+	}
+	creds, err := p.getCredentials()
+	if err != nil {
+		return nil, err
+	}
+	return p.requestCertificate(creds)
+}
+
+func (p *Params) getRoleCertificateTLS() (*tls.Certificate, error) {
+	certPEM, err := p.getRoleCertificate()
+	if err != nil {
+		return nil, err
+	}
+	block, _ := pem.Decode(certPEM)
+	if block == nil {
+		return nil, fmt.Errorf("unable to decode certificate PEM block")
+	}
+	if block.Type != "CERTIFICATE" {
+		return nil, fmt.Errorf("invalid certificate type: %s", block.Type)
+	}
+	x509Cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	return &tls.Certificate{
+		Certificate: [][]byte{block.Bytes},
+		PrivateKey:  p.Signer,
+		Leaf:        x509Cert,
+	}, nil
+}
+
+// Returns certificate PEM block.
+func (p *Params) requestCertificate(creds aws.Credentials) ([]byte, error) {
 	hostPath := p.KeymasterServer + paths.RequestAwsRoleCertificatePath
 	body := &bytes.Buffer{}
-	fmt.Fprintf(body, "aws_access_key_id     = %s\n", key)
-	fmt.Fprintf(body, "aws_secret_access_key = %s\n", secret)
-	fmt.Fprintf(body, "aws_session_token     = %s\n", token)
+	fmt.Fprintf(body, "aws_access_key_id     = %s\n", creds.AccessKeyID)
+	fmt.Fprintf(body, "aws_secret_access_key = %s\n", creds.SecretAccessKey)
+	fmt.Fprintf(body, "aws_session_token     = %s\n", creds.SessionToken)
 	body.Write(p.pemPubKey)
 	req, err := http.NewRequestWithContext(p.Context, "GET", hostPath, body)
 	if err != nil {
@@ -132,4 +120,62 @@ func (p *Params) requestCertificate(key, secret, token string) ([]byte, error) {
 			resp.Status, hostPath)
 	}
 	return ioutil.ReadAll(resp.Body)
+}
+
+func (p *Params) setupVerify() error {
+	if p.isSetup {
+		return nil
+	}
+	if p.KeymasterServer == "" {
+		return fmt.Errorf("no keymaster server specified")
+	}
+	if p.Logger == nil {
+		return fmt.Errorf("no logger specified")
+	}
+	if p.Context == nil {
+		p.Context = context.TODO()
+	}
+	if p.HttpClient == nil {
+		p.HttpClient = http.DefaultClient
+	}
+	if p.Signer == nil {
+		signer, err := rsa.GenerateKey(rand.Reader, 2048)
+		if err != nil {
+			return err
+		}
+		p.Signer = signer
+		p.KeyType = "RSA"
+	}
+	if p.KeyType != "RSA" {
+		return fmt.Errorf("unsupported key type: %s", p.KeyType)
+	}
+	derPubKey, err := x509.MarshalPKIXPublicKey(p.Signer.Public())
+	if err != nil {
+		return err
+	}
+	p.derPubKey = derPubKey
+	p.pemPubKey = pem.EncodeToMemory(&pem.Block{
+		Bytes: p.derPubKey,
+		Type:  "PUBLIC KEY",
+	})
+	awsConfig, err := config.LoadDefaultConfig(p.Context)
+	if err != nil {
+		return err
+	}
+	p.awsConfig = awsConfig
+	p.stsClient = sts.NewFromConfig(awsConfig)
+	idOutput, err := p.stsClient.GetCallerIdentity(p.Context,
+		&sts.GetCallerIdentityInput{})
+	if err != nil {
+		return err
+	}
+	p.Logger.Debugf(0, "Account: %s, ARN: %s, UserId: %s\n",
+		*idOutput.Account, *idOutput.Arn, *idOutput.UserId)
+	parsedArn, err := parseArn(*idOutput.Arn)
+	if err != nil {
+		return err
+	}
+	p.roleArn = parsedArn.String()
+	p.isSetup = true
+	return nil
 }
