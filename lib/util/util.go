e1ea3419183b7cbb0227cbe9f5f0ead480b5cdfe
Camilo Viecco <cviecco@purestorage.com>
2021-09-21 15:16:02 -0700

    adding source ip to certgen log message

--- lib/util/util.go
+++ lib/util/util.go
@@ -5,9 +5,35 @@ import (
 	"io"
 	"log"
 	"mime/multipart"
+	"net"
 	"net/http"
 	"strings"
 )
+
+func GetRequestRealIp(r *http.Request) string {
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		ip = r.RemoteAddr
+	}
+	if ip != "127.0.0.1" {
+		return ip
+	}
+	// Check if behide nginx or apache
+	xRealIP := r.Header.Get("X-Real-Ip")
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+
+	for _, address := range strings.Split(xForwardedFor, ",") {
+		address = strings.TrimSpace(address)
+		if address != "" {
+			return address
+		}
+	}
+
+	if xRealIP != "" {
+		return xRealIP
+	}
+	return ip
+}
 
 func CreateSimpleDataBodyRequest(method string, urlStr string, filebytes []byte, contentType string) (*http.Request, error) {
 	bodyBuf := bytes.NewBuffer(filebytes)
